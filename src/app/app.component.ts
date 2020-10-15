c6ef345f70b3c4be035b6e2cf0882ad6d9589317
mgumieniak <maciejgumieniak007@gmail.com>
2020-10-15 23:26:48 +0200

    Dynamic <ng-template> rendering with ViewContainerRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,25 @@
-import {AfterViewInit, Component, ComponentFactoryResolver, ComponentRef, OnDestroy, ViewChild, ViewContainerRef} from '@angular/core';
-import {AuthFormComponent} from './auth-form/auth-form.component';
-import {User} from './auth-form/auth-form.interface';
+import {AfterViewInit, Component, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <button (click)="destroyComponent()">
-        Destroy
-      </button>
-      <button (click)="moveComponent()">
-        Move
-      </button>
-      <div #entry></div>
+      <ng-container #entry></ng-container>
+      <ng-template #tmpl>
+        Todd Motto : England, UK
+      </ng-template>
     </div>
   `
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent implements AfterViewInit {
 
   @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
+  @ViewChild('tmpl') tmpl: TemplateRef<any>;
 
-  constructor(
-    private resolver: ComponentFactoryResolver
-  ) {
-  }
-
-  private compRef: ComponentRef<AuthFormComponent>;
 
   ngAfterViewInit(): void {
-    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.entry.createComponent(authFormFactory)
-      .changeDetectorRef.detectChanges();
-
-    this.compRef = this.entry.createComponent(authFormFactory, 0);
-    this.compRef.instance.title = 'Create account';
-    this.compRef.instance.submitted.subscribe(this.loginUser);
-    this.compRef.changeDetectorRef.detectChanges();
+    const view = this.tmpl.createEmbeddedView(null);
+    this.entry.insert(view);
   }
 
-  loginUser = (user: User): void => {
-    console.log('Login', user);
-  };
-
-  destroyComponent = (): void => {
-    if (this.compRef) {
-      this.compRef.destroy();
-    }
-  };
-
-  moveComponent = () => {
-    this.entry.move(this.compRef.hostView, 1);
-  };
-
-  ngOnDestroy(): void {
-    if (this.compRef) {
-      this.compRef.destroy();
-    }
-  }
 }
