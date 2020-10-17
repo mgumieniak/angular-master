4bcae9cf81306481b91e5928b72ad19c0e6a8b9a
mgumieniak <maciejgumieniak007@gmail.com>
2020-10-17 19:10:08 +0200

    @HostListener and host Object  && Understanding @HostBinding

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,9 +1,12 @@
-import {Directive, HostListener} from '@angular/core';
+import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
+  @HostBinding('style.border')
+  border: string;
+
   @HostListener('input', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
@@ -13,12 +16,16 @@ export class CreditCardDirective {
       trimmed = trimmed.substr(0, 16);
     }
 
-    const numbers = [];
+    let numbers = [];
     for (let i = 0; i < trimmed.length; i += 4) {
       numbers.push(trimmed.substr(i, 4));
     }
 
     input.value = numbers.join(' ');
 
+    this.border = '';
+    if (/[^\d]+/.test(trimmed)) {
+      this.border = '1px solid red';
+    }
   }
 }
