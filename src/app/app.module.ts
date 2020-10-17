f1e51f02c1462541d26d1092aacfcd612e857a41
mgumieniak <maciejgumieniak007@gmail.com>
2020-10-17 19:51:19 +0200

    Creating a custom structural Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { MyForDirective } from './my-for/my-for.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardDirective
+    MyForDirective
   ],
   imports: [
     BrowserModule
